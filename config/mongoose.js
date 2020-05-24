99b5fe0e20b6a76a9a59e8cee71e0eb57543f9e2
carey <schiafang@gmail.com>
2020-05-24 17:56:05 +0800

    fix: mongoose error; feat: add generate route

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
+const db = mongoose.connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/urlRecord-list'
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
-
-db.on('error', console.log('MongoDB connection error:'))
+db.on('error', () => console.log('MongoDB connection error'))
 db.once('open', () => console.log('MongoDB connected!'))
 
 module.exports = db
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const UrlRecord = require('../../models/urlRecord')
+const randomUrl = require('../../public/javascripts/random')
 
 router.get('/', (req, res) => {
   return UrlRecord.find()
@@ -9,6 +10,24 @@ router.get('/', (req, res) => {
       res.render('index')
     })
     .catch(error => console.log('error'))
+})
+
+router.get('/:shortener', (req, res) => {
+  const shortener = req.params.shortener
+  return UrlRecord.find({ shortener: shortener })
+    .lean()
+    .then(result => {
+      res.redirect(`${result[0].link}`)
+    })
+    .catch(error => console.log('error!'))
+})
+
+router.post('/generate', (req, res) => {
+  const link = req.body.name
+  const shortener = randomUrl
+  const urlRecord = new UrlRecord({ link, shortener })
+  return urlRecord.save()
+    .then(() => res.render('generate', { link, shortener }))
 })
 
 module.exports = router
